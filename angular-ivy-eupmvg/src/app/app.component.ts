a867b8034ca6714487e7c8f14db852dd5149aa8d
rajani-meritgroup <61788781+rajani-meritgroup@users.noreply.github.com>
2021-02-18 22:18:24 +0530

    angular-ivy-eupmvg Initial Commit

---
+++ angular-ivy-eupmvg/src/app/app.component.ts
@@ -0,0 +1,29 @@
+/*
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
